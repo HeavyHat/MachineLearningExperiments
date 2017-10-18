a8fcc3055106cd299fa78b699e3cb0052e2c4aa6
Heavyhat <joshua.langley.1995@gmail.com>
2017-10-18 13:37:02 +0100

    Fixed arg parse and deprecation warning.

--- plotting.py
+++ plotting.py
@@ -1,5 +1,7 @@
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 from sklearn.base import clone
+from scipy.interpolate import interp2d
 import numpy as np
 
 
@@ -12,14 +14,13 @@ class BoundaryPlotter:
     def plot(self,training_x, testing_x):
         fulldata = training_x + testing_x
         x_min, x_max, y_min, y_max = self.__get_min_max(fulldata)
-        x_resolution = (x_max - x_min) / self.resolution
-        y_resolution = (y_max - y_min) / self.resolution
+        x_resolution, y_resolution = self.__get_resolutions(x_min, x_max, y_min, y_max)
         xx, yy = np.meshgrid(np.arange(x_min, x_max, x_resolution),
-                             np.arange(y_min, y_max, y_resolution))
+                    np.arange(y_min, y_max, y_resolution))
         figure, axarr = plt.subplots()
         z = self.model.predict(np.c_[xx.ravel(), yy.ravel()])
         z = z.reshape(xx.shape)
-        axarr.contourf(xx, yy, z, alpha=0.4)
+        axarr.contourf(xx, yy, z, alpha=0.3)
         return figure, axarr
 
     def __get_min_max(self, data):
@@ -29,3 +30,34 @@ class BoundaryPlotter:
         x_min, x_max = min(x) - 0.1 * max(x), max(x) + 0.1 * max(x)
         y_min, y_max = min(y) - 0.1 * max(y), max(y) + 0.1 * max(y)
         return x_min, x_max, y_min, y_max
+
+    def __get_resolutions(self, x_min, x_max, y_min, y_max):
+        x_resolution = (x_max - x_min) / self.resolution
+        y_resolution = (y_max - y_min) / self.resolution
+        return x_resolution, y_resolution
+
+
+class Landscape3DPlotter:
+
+    def __init__(self, resolution, x_function, y_function, z_function):
+        self.resolution = resolution
+        self.x_function = x_function
+        self.y_function = y_function
+        self.z_function = z_function
+
+
+    def plot(self, model_function):
+        X = self.x_function(range(self.resolution))
+        Y = self.y_function(range(self.resolution))
+        Z = list()
+        for x in X:
+            for y in Y:
+                model, = model_function(x, y)
+                z = self.z_function(model, x, y)
+                Z.append(z)
+        fig = plt.figure()
+        ax = fig.gca(projection='3d')
+        ax.plot_surface(X, Y, Z)
+        return ax
+
+
--- svmboundary.py
+++ svmboundary.py
@@ -1,6 +1,6 @@
 import pandas as pd
 from sklearn.datasets import *
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.svm import SVC
 import sklearn
 if sklearn.__version__ < '0.18':
     from sklearn.cross_validation import train_test_split
@@ -14,10 +14,10 @@ from plotting import BoundaryPlotter
 
 def parse_arguments():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--count', '-c', dest='count', metavar='C', type=int, nargs='+', default=1000)
-    parser.add_argument('--noise', '-n', dest='noise', metavar='N', type=float, nargs='+', default=0.2)
-    parser.add_argument('--ecount', '-e', dest='ecount', metavar='N', type=int, nargs='+', default=10)
-    parser.add_argument('--dataset', '-d', dest='dataset', metavar='N', type=object, nargs='+')
+    parser.add_argument('--count', '-c', dest=count', metavar='C', type=int, default=1000)
+    parser.add_argument('--noise', '-n', dest='noise', metavar='N', type=float, default=0.2)
+    parser.add_argument('--ecount', '-e', dest='ecount', metavar='N', type=int, default=10)
+    parser.add_argument('--dataset', '-d', dest='dataset', metavar='N', type=object)
     args = parser.parse_args()
     return args
 
@@ -27,10 +27,8 @@ def get_dataset_from_generator(count, noise, data_generator='make_moons'):
     return data
 
 
-def get_model_definition(n_count, criterion='gini', type=None):
-    if type is None:
-        return RandomForestClassifier(n_estimators=n_count, criterion=criterion)
-    return None
+def get_model_definition(C, gamma):
+    return SVC(C=C, gamma=gamma)
 
 if __name__ == '__main__':
     args = parse_arguments()
@@ -38,10 +36,10 @@ if __name__ == '__main__':
         data = get_dataset_from_generator(args.count, args.noise, data_generator=args.dataset)
     else:
         data = get_dataset_from_generator(args.count, args.noise)
-    forestClassifier = get_model_definition(args.ecount)
+    forestClassifier = get_model_definition(10, 4)
     training_X, testing_X, training_y, testing_y = train_test_split(data[0], data[1], test_size=0.5)
     forestClassifier.fit(training_X, training_y)
-    plotter = BoundaryPlotter(forestClassifier, resolution=2000)
+    plotter = BoundaryPlotter(forestClassifier, resolution=1000)
     colours = [(1 - target, 0, target) for target in testing_y]
     figure, plot = plotter.plot(training_X, testing_X)
     plot.scatter(testing_X[:, 0], testing_X[:, 1], c=colours)
